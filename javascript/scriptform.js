a46164bbcf63c50f6c244b00ec02e3e84333de35
Lucas Lima <luc.tech3337@gmail.com>
2020-05-05 18:18:40 -0300

    scriptform.js

---
+++ javascript/scriptform.js
@@ -0,0 +1,22 @@
+var but3 = window.document.querySelector('button#but3')
+var but4 = window.document.querySelector('button#but4')
+but3.addEventListener('mouseenter', entrarb3)
+but3.addEventListener('mouseout', sairb3)
+but4.addEventListener('mouseenter', entrarb4)
+but4.addEventListener('mouseout', sairb4)
+function entrarb3(){
+    but3.style.backgroundColor = '#0984e3'
+    but3.style.color = 'white'
+}
+function sairb3(){
+    but3.style.backgroundColor = ''
+    but3.style.color = ''
+}
+function entrarb4(){
+    but4.style.backgroundColor = '#0984e3'
+    but4.style.color = 'white'
+}
+function sairb4(){
+    but4.style.backgroundColor = ''
+    but4.style.color = ''
+}
